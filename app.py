a55dacee74c3f1d8ea5f59005168ed19a4ccf1b6
jbretondelacierva <201810388@alu.comillas.edu>
2023-12-21 17:39:31 +0100

    Commit inicial

--- app.py
+++ app.py
@@ -12,42 +12,43 @@ import requests
 import pandas as pd
 import dash
 import plotly.figure_factory as ff
-
-# Lectura de datos
-data=pd.read_csv('datos_examen.csv')
-
-# Definicion de variables
-list_vars=list(data.columns)[1:10]
-
-# Definicion de funciones
-def graf_dispersion_color_mean(data, var1, var2, var3):
-    fig = px.scatter(data, 
-                     x=var1, 
-                     y=var2,
-                     color=var3,
-                     color_continuous_scale=px.colors.sequential.Blugrn,
-                     hover_data=["country"])
-    
-    fig.add_vline(x=np.mean(data[[var1]])[0], opacity=1, line_width=0.75, line_dash='dash', line_color='skyblue')
-    fig.add_hline(y=np.mean(data[[var2]])[0], opacity=1, line_width=0.75, line_dash='dash', line_color='skyblue')
-    
-    fig.update_layout(template='plotly_white')
-    
-    return fig
-
-
-
-# Build of the app
 app = dash.Dash(__name__, 
                 external_stylesheets=[dbc.themes.SPACELAB], 
                 meta_tags=[{"name": "viewport", "content": "width=device-width"}],
                 suppress_callback_exceptions=True)
 
+# Lectura de datos
+bank_df = pd.read_csv("bank-full.csv", delimiter=";")
+bank_df.dropna(inplace=True)
+bank_df['job'] = bank_df['job'].astype('category')
+bank_df['marital'] = bank_df['marital'].astype('category')
+bank_df['education'] = bank_df['education'].astype('category')
+bank_df['default'] = bank_df['default'].astype('category')
+bank_df['housing'] = bank_df['housing'].astype('category')
+bank_df['loan'] = bank_df['loan'].astype('category')
+bank_df['day'] = bank_df['day'].astype('category')
+bank_df['month'] = bank_df['month'].astype('category')
+bank_df['poutcome'] = bank_df['poutcome'].astype('category')
+bank_df['y'] = bank_df['y'].astype('category')
+bank_df['y_bool'] = bank_df['y'].apply(lambda x: 1 if x == 'yes' else 0)
+bank_df['y_bool'] = bank_df['y_bool'].astype('int')
+
+# Convert string columns into dummy variables
+string_columns = ['job', 'marital', 'education', 'default', 'housing', 'loan', 'day', 'month', 'poutcome','contact']
+bank_df = pd.get_dummies(bank_df, columns=string_columns)
+
+bank_df.dtypes
+bank_df
+
+
+# Build of the app
+
+
 # Definir el layout de la página inicial
 app.layout = html.Div([
     html.Div([
         dbc.NavbarSimple(
-            brand="Descriptivo y clustering de paises con variables socio-económicas y de salud",
+            brand="Analisis de las campañas de marketing y futuras mejoras a futuro",
             color="#3F556A",
             dark=True
         ), 
@@ -55,110 +56,38 @@ app.layout = html.Div([
     dbc.Container([
         dbc.Row([
             dbc.Col([
-                html.H6("Comparativa triple de variables",style={'margin-top':'40px','font-family': 'Arial Black', 'color':'#6C6C6C'}),
-                html.Hr(style={'border': '1px solid gray','border-radius': '10px'}),
-                dcc.Dropdown(
-                    list_vars,
-                    placeholder="Select a variable", id='dropdown1',value='child_mort',
-                    style={'margin-top':'40px','margin-bottom':'60px','border-width':'2px','border-color':'#a0a3a2',
-                           'font-family': "Times New Roman",'border-radius': '5px','width':'50%'}
-                    ),
-                dcc.Dropdown(
-                    list_vars,
-                    placeholder="Select a variable", id='dropdown2', value='exports',
-                    style={'margin-top':'60px','margin-bottom':'60px','border-width':'2px','border-color':'#a0a3a2',
-                           'font-family': "Times New Roman",'border-radius': '5px','width':'50%'}
-                    ),
-                dcc.Dropdown(
-                    list_vars,
-                    placeholder="Select a variable", id='dropdown3', value='health',
-                    style={'margin-top':'60px','margin-bottom':'50px','border-width':'2px','border-color':'#a0a3a2',
-                           'font-family': "Times New Roman",'border-radius': '5px','width':'50%'}
-                    ),
-                dcc.Graph(id='figura_1')
-            ]),
-            dbc.Col([
-                html.H6("Diagrama caja de variables seleccionadas",style={'margin-top':'40px','font-family': 'Arial Black', 'color':'#6C6C6C'}),
-                html.Hr(style={'border': '1px solid gray','border-radius': '10px'}),
-                html.Div([
-                    html.H6('First Select Value',style={'margin-top':'25px','font-family': 'Arial Black', 'color':'#6C6C6C'}),
-                    dcc.RangeSlider(min=0, max=500, value=[0, 200],id='range_slider1',className='slider_range',tooltip={"placement": "bottom", "always_visible": True})
-                ],style={'margin-top':'40px','margin-bottom':'5px','width':'50%'}),
-                html.Div([
-                    html.H6('Second Select Value',style={'margin-top':'25px','font-family': 'Arial Black', 'color':'#6C6C6C'}),
-                    dcc.RangeSlider(min=0, max=500, value=[0, 200],id='range_slider2',className='slider_range',tooltip={"placement": "bottom", "always_visible": True})
-                ],style={'margin-top':'5px','margin-bottom':'30px','width':'50%'}),
-                html.Div([
-                    html.H6('Third Select Value',style={'margin-top':'25px','font-family': 'Arial Black', 'color':'#6C6C6C'}),
-                    dcc.RangeSlider(min=0, max=500, value=[0, 200],id='range_slider3',className='slider_range',tooltip={"placement": "bottom", "always_visible": True})
-                ],style={'margin-top':'5px','margin-bottom':'30px','width':'50%'}),
-                dcc.Graph(id='figura_2')
-            ])
-        ]),
-        dbc.Row([
-            dbc.Col([
-                html.H6("Clustering en función de variables",style={'margin-top':'20px','font-family': 'Arial Black', 'color':'#6C6C6C'}),
-                html.Hr(style={'border': '1px solid gray','border-radius': '10px'}),
-                dcc.Dropdown(
-                    list_vars,
-                    placeholder="Select a variable", id='dropdown_clust1',value='health',
-                    style={'margin-top':'40px','margin-bottom':'10px','border-width':'2px','border-color':'#a0a3a2',
-                           'font-family': "Times New Roman",'border-radius': '5px','width':'50%'}
-                    ),
-                dcc.Dropdown(
-                    list_vars,
-                    placeholder="Select a variable", id='dropdown_clust2',value='gdpp',
-                    style={'margin-top':'10px','margin-bottom':'5px','border-width':'2px','border-color':'#a0a3a2',
-                           'font-family': "Times New Roman",'border-radius': '5px','width':'50%'}
-                    ),
-                dcc.Graph(id='figura_3'),
-                dcc.Textarea(
-                    value='Cluster0: High GDPP with high life expentancy and lower child mort.\nCluster1: Low GDPP with low life expentancy and medium high child mort.\nCluster2: Medium high GDPP with medium high life expentancy and low child mort.\nCluster3: Lowest GDPP with lowest life expentancy and highest child mort.\nCluster4: Low GDPP with medium low life expentancy and higher child mort.\nCluster5: Highest GDPP with highest life expentancy and lowest child mort.\nCluster6: Medium low GDPP with medium low life expentancy and medium low child mort.',
-                    style={'margin-top':'30px','width': '100%', 'height': '175px'},
+                html.H1('Exito en funcion de la edad', style={'textAlign': 'center'}),
+                    
+                # Componente para elegir qué predicciones mostrar
+                dcc.Checklist(
+                    id='selector_age',
+                    options=[
+                        {'label': 'Si', 'value': 'Si'},
+                        {'label': 'No', 'value': 'No'}
+                    ],
+                    value=['Si', 'No'],
                 ),
-            ]),
-            dbc.Col([
-                html.H6("PCA clustering de los datos",style={'margin-top':'20px','font-family': 'Arial Black', 'color':'#6C6C6C'}),
-                html.Hr(style={'border': '1px solid gray','border-radius': '10px','margin-bottom':'110px'}),
-                dcc.Graph(figure=graph_pca())
+                dcc.Graph(id='grafico_predicciones', style={'height': '600px'}),  # Aumentar altura de la gráfica
             ])
         ])
-    ])
+])
 ])
 
+
 # Definir callbacks
-@app.callback(Output('figura_1', 'figure'), 
-              [Input('dropdown1','value'),
-               Input('dropdown2','value'),
-               Input('dropdown3','value')]
+@callback(
+    Output('histogramAge', 'figure'),
+    [Input('selector_age', 'value')]  # Se activa con el cambio en el selector
 )
-def draw_figure_1(dropdown1,dropdown2,dropdown3):
-   fig=graf_dispersion_color_mean(data,dropdown1,dropdown2,dropdown3)
-   return fig
+def histogramAge(selector):
 
-@app.callback(Output('figura_2', 'figure'), 
-              [Input('range_slider1','value'),
-               Input('range_slider2','value'),
-               Input('range_slider3','value'),
-               Input('dropdown1','value'),
-               Input('dropdown2','value'),
-               Input('dropdown3','value')]
-)
-def draw_figure_2(range_slider1,range_slider2,range_slider3,dropdown1,dropdown2,dropdown3): 
-    data_aux=data.loc[(data[dropdown1]>=range_slider1[0])&(data[dropdown1]<=range_slider1[1])&
-                      (data[dropdown2]>=range_slider2[0])&(data[dropdown2]<=range_slider2[1])&
-                      (data[dropdown3]>=range_slider3[0])&(data[dropdown3]<=range_slider3[1])]
-    fig=cajas_graph(data_aux,dropdown1,dropdown2,dropdown3)
+    fig = px.histogram(bank_df, x="age", color="y",
+                    labels = {"streams": "Reproducciones"},
+                    histnorm='probability density',
+                    nbins = 100,
+                    opacity=0.6)
     return fig
 
-@app.callback(Output('figura_3', 'figure'), 
-              [Input('dropdown_clust1','value'),
-               Input('dropdown_clust2','value')]
-)
-def draw_figure_3(dropdown_clust1,dropdown_clust2):
-    fig=graph_clust(dropdown_clust1,dropdown_clust2)
-    return fig
 
-# Run de la aplicación
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run_server(debug=True)
